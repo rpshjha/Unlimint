923f484ccac41371f09b660cc40fe7029bb901fb
Rupesh Kumar <rupeshkumar@192.168.60.84>
2022-11-11 08:53:11 +0530

    initial commit

---
+++ unlimint/src/test/java/com/unlimint/AppTest.java
@@ -0,0 +1,16 @@
+package com.unlimint;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+
+    }
+}
